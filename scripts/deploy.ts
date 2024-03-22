48bc28533bc472ef47382da8e12962437d98b753
Nicco6598 <nicco6598@gmail.com>
2024-03-22 11:01:34 +0100

    ora vengono passati correttamente i contructor al momento del deploy (sostituire valori con quelli corretti)

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,14 +1,26 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  // Sostituisci 'MyNFT' e i parametri del costruttore come necessario
   const NFTContractFactory = await ethers.getContractFactory("MyNFT");
-  const subscriptionId = "10060"; 
-  const nftContract = await NFTContractFactory.deploy(subscriptionId);
+  const subscriptionId = 1; // Convertito da stringa a numero
+  const VRFCoordinatorV2 = "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625";
+  const gasLane = "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c";
+  const callbackGasLimit = 100000; // Placeholder, sostituire con il valore corretto
+  const mintFee = ethers.parseUnits("0.1", "ether"); // Convertito in wei
+  const maxTokens = 10000; // Placeholder, definire il numero massimo di token
 
-  await nftContract.deployed();
+  const nftContract = await NFTContractFactory.deploy(
+    VRFCoordinatorV2, 
+    subscriptionId, 
+    gasLane,
+    callbackGasLimit,
+    mintFee,
+    maxTokens
+  );
 
-  console.log("MyNFT deployed to:", nftContract.address);
+  await nftContract.waitForDeployment();
+
+  console.log("MyNFT deployed to:", nftContract.target);
 }
 
 main().catch((error) => {
--- test/MyNFT.test.ts
+++ test/MyNFT.test.ts
@@ -7,7 +7,13 @@ describe('MyNFT', function () {
   let MyNFT: MyNFT;
   let vrfCoordinatorV2Mock: VRFCoordinatorV2Mock;
   let owner: SignerWithAddress, addr1: SignerWithAddress;
-  const subscriptionId = 1;
+  const NFTContractFactory = await ethers.getContractFactory("MyNFT");
+  const subscriptionId = 1; // Convertito da stringa a numero
+  const VRFCoordinatorV2 = "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625";
+  const gasLane = "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c";
+  const callbackGasLimit = 100000; // Placeholder, sostituire con il valore corretto
+  const mintFee = ethers.parseUnits("0.1", "ether"); // Convertito in wei
+  const maxTokens = 10000; // Placeholder, definire il numero massimo di token
   const chainId = network.config.chainId!;
 
   beforeEach(async function () {
@@ -18,7 +24,15 @@ describe('MyNFT', function () {
     ]);
     await vrfCoordinatorV2Mock.waitForDeployment();
 
-    MyNFT = await ethers.deployContract('MyNFT');
+    const NFTContractFactory = await ethers.getContractFactory("MyNFT");
+    const MyNFT = await NFTContractFactory.deploy(
+      VRFCoordinatorV2, 
+      subscriptionId, 
+      gasLane,
+      callbackGasLimit,
+      mintFee,
+      maxTokens
+    );
     await MyNFT.waitForDeployment();
   });
 
